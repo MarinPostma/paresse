c3663ad51014958cb8eb132c6c9a4c4f7f486247
ad hoc <postma.marin@protonmail.com>
2024-10-18 12:24:26 +0200

    add bitset intersection op overload

--- src/bitset/ops/intersection.rs
+++ src/bitset/ops/intersection.rs
@@ -1,4 +1,6 @@
-use crate::bitset::{bitset_like::BitSetLike, BlockRepr};
+use std::ops::BitAnd;
+
+use crate::bitset::{bitset_like::BitSetLike, BitSet, BlockRepr};
 
 pub struct Intersection<A, B> {
     a: A,
@@ -14,7 +16,7 @@ where
         let mut iter_a = self.a.blocks();
         let mut iter_b = self.b.blocks();
 
-        std::iter::from_fn(move || match dbg!((iter_a.next(), iter_b.next())) {
+        std::iter::from_fn(move || match (iter_a.next(), iter_b.next()) {
             (Some(a), Some(b)) => Some(a & b),
             _ => None,
         })
@@ -28,5 +30,29 @@ where
 impl<A, B> Intersection<A, B> {
     pub fn new(a: A, b: B) -> Self {
         Self { a, b }
+    }
+}
+
+impl<B> BitAnd<B> for &BitSet
+where
+    B: BitSetLike,
+{
+    type Output = BitSet;
+
+    fn bitand(self, rhs: B) -> Self::Output {
+        self.intersection(rhs).collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::bitset;
+
+    #[test]
+    fn intersection() {
+        let a = bitset![1, 3, 7, 999, 521];
+        let b = bitset![2, 7, 999, 523, 521];
+
+        assert_eq!(&a & &b, bitset![7, 999, 521]);
     }
 }
